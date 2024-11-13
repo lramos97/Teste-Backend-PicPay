fb201fcbdeaca3140b9b70c64e373b2432e85812
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-13 17:34:36 -0300

    feat: Implementing unit tests

---
+++ src/test/java/com/picpaysimplificado/picpaysimplificado/repositories/UserRepositoryTest.java
@@ -0,0 +1,58 @@
+package com.picpaysimplificado.picpaysimplificado.repositories;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.picpaysimplificado.dtos.UserDTO;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.assertj.core.api.AbstractBooleanAssert;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    @DisplayName("Should get user successfully from DB")
+    void findUserByDocumentCase1() {
+        String document = "999999999";
+        UserDTO data = new UserDTO("Larissa", "Ramos", document, new BigDecimal(16), "test@gmail.com", "1234", UserType.COMMON);
+        this.createUser(data);
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should not get user from DB when user not exists")
+    void findUserByDocumentCase2() {
+        String document = "999999999";
+
+        Optional<User> result = this.userRepository.findUserByDocument(document);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    private User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.entityManager.persist(newUser);
+        return newUser;
+    }
+}
