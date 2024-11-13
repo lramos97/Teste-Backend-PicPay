72d6d80e22f6f05afbe9bf12522807ff84fecd74
Larissa O Ramos <larissadeoliveiraramos@hotmail.com>
2024-11-13 15:48:05 -0300

    feat: create Notification DTO, Service

---
+++ src/main/java/com/picpaysimplificado/picpaysimplificado/services/NotificationService.java
@@ -0,0 +1,22 @@
+package com.picpaysimplificado.picpaysimplificado.services;
+
+import com.picpaysimplificado.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.picpaysimplificado.dtos.NotificationDTO;
+import jdk.jfr.Experimental;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        System.out.println("Notificação enviada ao usuário");
+    }
+}
